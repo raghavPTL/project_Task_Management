2c31585b18a7f16e63da13ccc7dc68d1aa31cf4a
RAGHAV PATEL <raghavnpatel@RAGHAVs-MacBook-Air.local>
2023-07-04 23:43:25 +0530

    add env file and home page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const bodyPareser=require("body-parser");
 const mongoose=require("mongoose");
@@ -9,8 +10,7 @@ const _ = require("lodash");
 app.set('view engine', 'ejs');// use ejs as view engine
 app.use(bodyPareser.urlencoded({extended:true}));//to get data from html in dom
 app.use(express.static("public"));//some extra file load in server....
-//const DB=mongodb+srv://RaghavPatel:ragh123@cluster0.n1yemjk.mongodb.net/todoDB?retryWrites=true&w=majority
-mongoose.connect("mongodb+srv://RaghavPatel:ragh123@cluster0.n1yemjk.mongodb.net/todoDB").then(()=>{
+mongoose.connect(process.env.mongoUrl).then(()=>{
 console.log("connection sucess fully");
 }).catch((err)=>console.log("connectio FAILid"));
 //mongoose.connect("mongodb://localhost:27017/todoDB");
@@ -18,140 +18,101 @@ console.log("connection sucess fully");
 
 
 //let items=["wake up","bath","breakfat","reading news_paper"];//main todo list
-let workitems=[];
-const itemSchema={
-    name:String
-}
-const Item=mongoose.model("Item",itemSchema);
 
-const item1=new Item({
-    name:"welcome to todolist"
-})
-const item2=new Item({
-    name:"Enter + button to add new list-item"
-})
-const item3=new Item({
-    name:"click check-box to delete your  item form list"
-})
-const defaultitems=[item1,item2,item3];
 const listSchema={
     name:String,
-    itemsofsaperateList:[itemSchema]
+    itemsofsaperateList:[]
 }
 const List=mongoose.model("List",listSchema);
 
 
 
 
-//home route   start journey from here...
+
 app.get("/",function(req,res){
-        let day=Date.getDay();           //also  use Date.geteDate()
-         
-        Item.find({},function(err,founditems){
-             //console.log(founditems);
-             if(founditems.length==0)
-             {
-                Item.insertMany(defaultitems,function(err){
-                        if(err){
-                                 console.log(err);
-                                }
-                        else{
-                            console.log("default items push sucessfully");
-                            }
-                 })
-                //res.redirect("/");
-                res.render("list", {listtitle: "Today", newlistitems:founditems});
-             }
-             else{
-                  res.render("list", {listtitle: "Today", newlistitems:founditems});
-             }
+    List.find()
+    .then((lists) => {
+      res.render('home', { lists : lists});
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    });
     
-        })
+
+          
 
 });
- 
-app.get("/:coustomListName",function(req,res){
-    const coustomlistname=_.capitalize(req.params.coustomListName);
 
 
-
-    List.findOne({name:coustomlistname},function(err,foundlist){
-        if(!err){//
-            if(foundlist){
-                console.log("Exist");
-                res.render("list", {listtitle:foundlist.name , newlistitems:foundlist.itemsofsaperateList});
-               
-            }
-            else{
-                 console.log("Not Exist");
-                 const newpage=new List({
-                         name:coustomlistname,
-                         itemsofsaperateList:defaultitems
-                    })
-                  newpage.save();
-                  res.redirect("/"+coustomlistname);
-                  
-
-            }
-        }
+app.get('/but', (req, res) => {
+   List.find()
+    .then((lists) => {
+      res.render('home', { lists : lists});
     })
-  
-})
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    });
+});
+
+
+app.get("/posts/:postid",function(req,res){
+     
+     
+      const requestedPostId = req.params.postid;
+      List.findOne({_id: requestedPostId}, function(err, postt){
+      
+         console.log(postt);
+         res.render("list", { cur:postt});
+      
+       });
+      
+
+});
 
 
 
-
-
-
-
-
-app.post("/", function (req, res) {
- 
-    //console.log(req.body);
-    const itemName=req.body.newitem;
-    const listname=req.body.list;
-    const iteminmainpage=new Item({
-        name:itemName
-    })
-    if(listname=="Today")
-    {
-         iteminmainpage.save();
-         res.redirect("/");
-    }
-    else{
-           List.findOne({name:listname},function(err,foundlist){
-            foundlist.itemsofsaperateList.push(iteminmainpage);
-            foundlist.save();
-            res.redirect("/"+foundlist.name);
-           })
-    }
-
-    //iteminmainpage.save();
-    //res.redirect("/");
-})
 
 app.post("/delete",function(req,res){
-   const deleteid=req.body.checkbox.trim();
-   const nameofpage=req.body.ListName;
-   if(nameofpage=="Today"){
-           Item.findByIdAndRemove(deleteid,function(err){
-                if(err){
-                   console.log(err);
-                }
-                else{
-                   console.log("delete sucessfully")
-                    res.redirect("/");
-                }
-           })
-   }
-   else {
-    List.findOneAndUpdate({name: nameofpage}, {$pull: {itemsofsaperateList: {_id: deleteid}}}, function(err, foundList){
+   const deletename=req.body.checkbox.trim();
+   const idofpage=req.body.pageid;
+   
+    List.findOneAndUpdate({_id: idofpage}, {$pull: {itemsofsaperateList: {name: deletename}}}, function(err, foundList){
       if (!err){
-        res.redirect("/" + nameofpage);
+       console.log("delete sucess fully ");
+      const requestedPostId = idofpage;
+      console.log(deletename);
+      List.findOne({_id: requestedPostId}, function(err, postt){
+      
+         console.log(postt);
+         res.render("list", { cur:postt});
+      
+       });
+
+
       }
     });
-  }
+   
 
+})
+
+app.post("/delete_sec",function(req,res){
+   const deleteid=req.body.checkbox.trim();
+   
+    List.findOneAndDelete({ _id: deleteid })
+  .then((deletedList) => {
+    if (deletedList) {
+      console.log("Document deleted successfully:", deletedList);
+    } else {
+      console.log("Document not found.");
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+  
+    res.redirect("/");
 
     
    
@@ -162,6 +123,71 @@ app.post("/delete",function(req,res){
 
 
 
+
+
+app.post("/addnewtask", function (req, res) {
+ 
+    //console.log(req.body);
+    const itemName=req.body.newsec.toUpperCase();
+   // const listname=req.body.list;
+ 
+   const newList = new List({
+  name: itemName,
+  itemsofsaperateList: []
+});
+
+// Save the new instance to the database
+newList.save()
+  .then(() => {
+    console.log("New list instance created successfully.");
+    res.redirect("/");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+    
+
+    //iteminmainpage.save();
+    //res.redirect("/");
+})
+
+
+app.post("/addnewgoal", function (req, res) {
+ 
+    const listId = req.body.sub;
+const newItem = {
+ 
+  name:  capitalizeFirstLetter(req.body.newgoal),
+  // Additional properties of the item
+};
+
+// Find the document by list ID and push the new element to itemsofsaperateList
+List.findOneAndUpdate(
+  { _id: listId },
+  { $push: { itemsofsaperateList: newItem } },
+  { new: true }
+)
+  .then((updatedList) => {
+    if (updatedList) {
+      console.log("New element added successfully:", updatedList);
+      const requestedPostId = listId;
+      List.findOne({_id: requestedPostId}, function(err, postt){
+      
+         console.log(postt);
+         res.render("list", { cur:postt});
+      
+       });
+
+
+    } else {
+      console.log("List not found.");
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+    
+})
 
 
 //app.get("/work",function(req,res){
@@ -176,6 +202,9 @@ app.post("/delete",function(req,res){
 //    res.redirect("/work");
 //})
 
+function capitalizeFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
 
 
 
@@ -237,3 +266,26 @@ app.listen(port,function()
     <% } else{ %>
          <h1 style="color:green;">today is <%=kindofday%></h1>
    <% } %> */
+
+
+
+
+
+   // <li><%= item.name %></li>
+
+
+
+
+/*
+   <%for(var i=0;i < list.length ;i++){ %>
+         <form action="/delete" method="post"autocomplete="false" >
+        <div class="item">
+          
+            <input type="checkbox" name="checkbox" value="<%=list[i].name %>" onChange="this.form.submit()">
+           
+            <a href = "/posts/<%=list[i].name %> " ><p> <%= list[i].name %> </p></a>  
+         
+          
+        </div>
+        </form>
+        <%}%> */
